40f9a09a83257a674a5c43f30179a13f85cf9b42
Alberto Ferrini <alberto.ferrini.mail@gmail.com>
2017-05-30 10:18:27 +0200

    finished lesson 185

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,44 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private defaultQuestion = 'pet';
+  private answer = '';
+  private genders = ['male', 'female'];
+  private submitted = false;
+  private user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+
   @ViewChild('f') private form: NgForm;
   suggestUserName() {
     const suggestedName = 'Superuser';
+    /*    this.form.setValue({
+          userData: {
+            username: suggestedName,
+            email: ''
+          },
+          secret: 'pet',
+          questionAnswer: '',
+          gender: 'male'
+        });*/
+    this.form.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   onSubmit() {
-    console.log(this.form);
+    this.user.username = this.form.value.userData.username;
+    this.user.email = this.form.value.userData.email;
+    this.user.secretQuestion = this.form.value.secret;
+    this.user.answer = this.form.value.questionAnswer;
+    this.user.gender = this.form.value.gender;
+    this.submitted = true;
+    this.form.reset();
   }
 }
